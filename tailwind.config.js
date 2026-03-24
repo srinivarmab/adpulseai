1a6db9e2d27b2c685f210743b0b900f445361d60
Srinivas Boreda <89035957+srinivarmab@users.noreply.github.com>
2026-03-24 14:16:07 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./lib/**/*.{js,ts,jsx,tsx}"
+    "./app/*/.{js,ts,jsx,tsx}",
+    "./components/*/.{js,ts,jsx,tsx}",
+    "./lib/*/.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {},
